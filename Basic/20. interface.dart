363bef1be3a04638d9ddf7f197a545e25a5ce6da
Ray <iinoshirozheng@gmail.com>
2022-09-23 00:14:15 +0800

    interface and inheritance class

--- Basic/20. interface.dart
+++ Basic/20. interface.dart
@@ -1,6 +1,31 @@
 class Phone {
-  void start() {}
+  void start() {
+    print("starting");
+  }
+
   void shutdown() {
     print("shutting");
   }
+}
+
+abstract class System {
+  String system = '';
+}
+
+class IPhone implements Phone, System {
+  String system = 'ios';
+
+  void start() {
+    print("start iphone");
+  }
+
+  void shutdown() {
+    print('end iphone');
+  }
+}
+
+void main(List<String> args) {
+  var myPhone = IPhone();
+  myPhone.start();
+  myPhone.shutdown();
 }
