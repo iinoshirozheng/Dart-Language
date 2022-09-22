97a14f805b8b9379fcfcb3154a1ed0f5a7b32659
Ray <iinoshirozheng@gmail.com>
2022-09-23 00:50:17 +0800

    extends

---
+++ Basic/21. extends.dart
@@ -0,0 +1,28 @@
+abstract class Phone {
+  void startUp();
+  void Shutdown() {}
+  String system;
+  Phone(this.system);
+}
+
+// implements must to instatiance all things in class
+// extends just need to intantiance virtual function
+class IOSPhone extends Phone {
+  IOSPhone() : super('ios');
+  void startUp() {
+    print('my iphone is $system System');
+  }
+
+  @override
+  void noSuchMethod(Invocation mirror) {
+    print('override noSuchMethod');
+  }
+}
+
+void main(List<String> args) {
+  IOSPhone phone = IOSPhone();
+  phone.startUp();
+
+  dynamic phone2 = IOSPhone();
+  phone2.hauisdh();
+}
