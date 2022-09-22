076769be00d03aca5532f78163dcbeb1a841e440
Ray <iinoshirozheng@gmail.com>
2022-09-22 23:05:06 +0800

    class (important)

---
+++ Basic/16. class.dart
@@ -0,0 +1,26 @@
+class Point {
+  num x = 0;
+  num y = 0;
+  var initNum;
+  // Point(int x, int y) {
+  //   this.x = x;
+  //   this.y = y;
+  // }
+
+  Point(this.x, this.y) : initNum = x + y;
+
+  Point.fromJSON(Map json) {
+    this.x = json['x'];
+    this.y = json['y'];
+  }
+}
+
+void main(List<String> args) {
+  var p = Point(11, 22);
+  print([p.x, p.y]);
+
+  // init from Map
+  Map myJson = {'x': 12, 'y': 24};
+  var myPoint = Point.fromJSON(myJson);
+  print([myPoint.x, myPoint.y]);
+}
