905e3655764c89217d60222d58ec00db71a50069
Ray <iinoshirozheng@gmail.com>
2022-09-23 01:20:21 +0800

    class extends update (inheritance)

---
+++ Basic/22. mixin.dart
@@ -0,0 +1,41 @@
+abstract class Phone {
+  void startUp() {
+    print('Start');
+  }
+
+  void shutdown() {
+    print('Shutdown');
+  }
+}
+
+abstract class System {
+  void whichSystem();
+}
+
+class IOSPhone extends Phone with System {
+  void whichSystem() {
+    print('IOS system');
+  }
+}
+
+class Line {
+  void sendMsg() {
+    print('line send!');
+  }
+}
+
+class Messenger {
+  void sendMsg() {
+    print('messenger send!');
+  }
+}
+
+class IPhone_PRO extends IOSPhone with Messenger, Line {}
+
+void main(List<String> args) {
+  var iphone = IPhone_PRO();
+  iphone.startUp();
+  iphone.whichSystem();
+  iphone.sendMsg();
+  iphone.shutdown();
+}
